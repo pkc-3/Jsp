83a118dfdaa8bc818ecbbf055954c3364a2dca24
pkc-3 <pkc_3@naver.com>
2021-06-23 12:41:38 +0900

    2021/06/23 Jsp 팜스토리 홈페이지 제작

---
+++ Farmstory1/src/main/java/kr/co/farmstory1/db/DBConfig.java
@@ -0,0 +1,42 @@
+package kr.co.farmstory1.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConfig {
+	
+	private static DBConfig instance = new DBConfig();
+	private DBConfig() {}
+	
+	public static DBConfig getInstance() {
+		return instance;
+	}
+		
+	//DB ����
+	private final String HOST = "jdbc:mysql://54.180.123.231:3306/pkc716054";
+	private final String USER = "pkc716054";
+	private final String PASS = "1234";
+	
+	public Connection getConnection() throws Exception {
+		// 1�ܰ�
+		Class.forName("com.mysql.jdbc.Driver");
+		// 2�ܰ�
+		Connection conn = DriverManager.getConnection(HOST, USER, PASS);
+		
+		return conn;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
